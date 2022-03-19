7d165f42b376ae7d882a6c8954b86cf88113f52c
Ishan Mondal <76674591+ishan-im@users.noreply.github.com>
2022-03-19 08:38:49 +0530

    update change index.js

--- index.js
+++ index.js
@@ -3,113 +3,109 @@
 
 var numberOfDrumbuttons = document.querySelectorAll(".drum").length;
 
-for(var i=0; i<numberOfDrumbuttons; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+for (var i = 0; i < numberOfDrumbuttons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
-var buttonInnerHTML= this.innerHTML;
+    var buttonInnerHTML = this.innerHTML;
 
 
-makeSound(buttonInnerHTML);
+    makeSound(buttonInnerHTML);
 
-buttonAnimation(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
 
   })
 }
 
 //detecting keypress
 
-document.addEventListener("keydown",function (event) {
+document.addEventListener("keydown", function (event) {
 
 
 
 
 
-    makeSound(event.key);
-    
-    buttonAnimation(event.key);
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
 });
 
 //sound function
 
-function makeSound(key){
-    switch (key) {
-      case "A":
-        
+function makeSound(key) {
+  switch (key) {
+    case "A", "a":
+
       var boom = new Audio("sounds/Drum_Kit_sounds_boom.wav");
       boom.play();
-  
-        break;
-  
-    case "S":
-        
-    var clap = new Audio("sounds/Drum_Kit_sounds_clap.wav");
-    clap.play();
-    break;
-  
-    case "D":
-        
-    var hihat = new Audio("sounds/Drum_Kit_sounds_hihat.wav");
-    hihat.play();
+
       break;
-  
-      case "F":
-        
+
+    case "S", "s":
+
+      var clap = new Audio("sounds/Drum_Kit_sounds_clap.wav");
+      clap.play();
+      break;
+
+    case "D", "d":
+
+      var hihat = new Audio("sounds/Drum_Kit_sounds_hihat.wav");
+      hihat.play();
+      break;
+
+    case "F", "f":
+
       var kick = new Audio("sounds/Drum_Kit_sounds_kick.wav");
       kick.play();
-        break;
-  
-        case "G":
-           
-        var openhat = new Audio("sounds/Drum_Kit_sounds_openhat.wav");
-        openhat.play();
-          break;
-  
-          case "H":
-            
-          var ride = new Audio("sounds/Drum_Kit_sounds_ride.wav");
-          ride.play();
-            break;
-  
-            case "J":
-                
-            var snare = new Audio("sounds/Drum_Kit_sounds_snare.wav");
-            snare.play();
-              break;
-        
-              case "K":
-               
-              var tink = new Audio("sounds/Drum_Kit_sounds_tink.wav")
-              tink.play();
-              break;
-           
-              case "L":
-                
-            var tom = new Audio("sounds/Drum_Kit_sounds_tom.wav")
-            tom.play();
-            break;  
-  
-      default:console.log(innerHTML);
-  
-    }
-  
+      break;
+
+    case "G", "g":
+
+      var openhat = new Audio("sounds/Drum_Kit_sounds_openhat.wav");
+      openhat.play();
+      break;
+
+    case "H", "h":
+
+      var ride = new Audio("sounds/Drum_Kit_sounds_ride.wav");
+      ride.play();
+      break;
+
+    case "J", "j":
+
+      var snare = new Audio("sounds/Drum_Kit_sounds_snare.wav");
+      snare.play();
+      break;
+
+    case "K", "k":
+
+      var tink = new Audio("sounds/Drum_Kit_sounds_tink.wav")
+      tink.play();
+      break;
+
+    case "L", "l":
+
+      var tom = new Audio("sounds/Drum_Kit_sounds_tom.wav")
+      tom.play();
+      break;
+
+    default: console.log(innerHTML);
+
   }
+
+}
 
 //animation function
 
-  function buttonAnimation(currentKey){
+function buttonAnimation(currentKey) {
 
-    var activeButton=  document.querySelector("." + currentKey);
-    activeButton.classList.add("button-onclick");
-    
-    setTimeout(
-      function(){
-        activeButton.classList.remove("button-onclick");
-      },100
-    );
-    
-    }
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("button-onclick");
 
-// function myFunction(eventKey){
-//   var x=eventKey.toUpperCase();
-//   return x;
-// }
+  setTimeout(
+    function () {
+      activeButton.classList.remove("button-onclick");
+    }, 100
+  );
+
+}
+
